c8f71294185e0c6ebe0d7e67f3804cdb24ed2afe
Opara19 <opararusandi@gmail.com>
2024-09-16 13:53:25 +0530

    no_tensorflow

--- lepto_new.py
+++ lepto_new.py
@@ -9,7 +9,7 @@ from xgboost import XGBRegressor
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
-import tensorflow as tf
+#import tensorflow as tf
 # from tf.keras.models import Sequential
 # from tf.keras.layers import LSTM, Dense
 from sklearn.preprocessing import MinMaxScaler
